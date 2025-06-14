600f790fc5e40886d93ea9fa548e4f992f06a545
softdeveloper23 <brannongarrett77@gmail.com>
2025-06-14 15:10:59 +0200

    Added imports

--- basic_ai_agent/main.py
+++ basic_ai_agent/main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from basic-ai-agent!")
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import asyncio
+import os
 
-
-if __name__ == "__main__":
-    main()
+load_dotenv()
